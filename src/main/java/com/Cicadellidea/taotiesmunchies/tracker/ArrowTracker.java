1796bc3963074e930deb2732286e588298a7a580
Cicadellidea <yezi3101@hotmail.com>
2024-09-21 15:12:11 +0800

    Update ArrowTracker.java
    可以让三叉戟吃到箭矢伤害
    但是药水箭等附加伤害不能和原版一样工作

--- src/main/java/com/Cicadellidea/taotiesmunchies/tracker/ArrowTracker.java
+++ src/main/java/com/Cicadellidea/taotiesmunchies/tracker/ArrowTracker.java
@@ -3,11 +3,17 @@ package com.Cicadellidea.taotiesmunchies.tracker;
 import com.Cicadellidea.taotiesmunchies.Capabilites.*;
 import com.Cicadellidea.taotiesmunchies.TaotiesMunchies;
 import com.Cicadellidea.taotiesmunchies.effect.ShootSpeedUp;
+import com.ibm.icu.text.CaseMap;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.projectile.Arrow;
+import net.minecraft.world.entity.projectile.ThrownTrident;
 import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.event.entity.ProjectileImpactEvent;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 import java.util.Objects;
@@ -17,61 +23,54 @@ public class ArrowTracker {
     public void onArrowSpawn(EntityEvent event)
     {
 
-        // Do something when the server starts
         Entity entity = event.getEntity();
 
             if (entity instanceof AbstractArrow arrow)
             {
-                if(!entity.level().isClientSide ) {
-                    arrow.getCapability(ArrowFoodAcceleratedProvider.ARROW_FOOD_ACCELERATED_CAPABILITY).ifPresent(acc ->
-                    {
-                        if (!acc.getFoodAccelerated()) {
-                            if(arrow.getOwner() instanceof  Player player)
+                if (arrow.tickCount == 0){
+                    if(!entity.level().isClientSide ) {
+                        if(arrow.getOwner() instanceof  Player player)
+                        {
+                            final double[] mul = {1};
+                            player.getCapability(PlayerFoodShootSpeedBonusProvider.PLAYER_FOOD_SHOOT_SPEED_BONUS_CAPABILITY).ifPresent(shootSpeedBonus->
                             {
-                                final double[] mul = {1};
-                                player.getCapability(PlayerFoodShootSpeedBonusProvider.PLAYER_FOOD_SHOOT_SPEED_BONUS_CAPABILITY).ifPresent(shootSpeedBonus->
-                                {
 
-                                    mul[0] = shootSpeedBonus.getActual()+ mul[0];
+                                mul[0] = shootSpeedBonus.getActual()+ mul[0];
 
-                                });
-                                MobEffect effect = TaotiesMunchies.SHOOT_SPEED_UP.get();
-                                if (player.hasEffect(effect)) {
-                                    mul[0] = mul[0] + (0.3*(player.getEffect(effect).getAmplifier()+1));
-                                }
-                                arrow.setDeltaMovement(arrow.getDeltaMovement().multiply(mul[0], mul[0], mul[0]));
-
-
-
-                                mul[0] = 1;
-                                player.getCapability(PlayerFoodArrowDamageBonusProvider.PLAYER_FOOD_ARROW_DAMAGE_BONUS_CAPABILITY).ifPresent(shootSpeedBonus->
-                                {
-                                    mul[0] = shootSpeedBonus.getActual()+mul[0];
-
-                                });
-                                effect = TaotiesMunchies.DMG_UP.get();
-                                if (player.hasEffect(effect)) {
-                                    mul[0] = mul[0] + (0.3*(player.getEffect(effect).getAmplifier()+1));
-                                }
-                                arrow.setBaseDamage(arrow.getBaseDamage()*mul[0]);
-
+                            });
+                            MobEffect effect = TaotiesMunchies.SHOOT_SPEED_UP.get();
+                            if (player.hasEffect(effect)) {
+                                mul[0] = mul[0] + (0.3*(player.getEffect(effect).getAmplifier()+1));
                             }
-                            acc.setFoodAccelerated(true);
-
+                            arrow.setDeltaMovement(arrow.getDeltaMovement().multiply(mul[0], mul[0], mul[0]));
                         }
+                    }
+                }
+            }
+    }
+    @SubscribeEvent
+    public void tridentHiting(LivingDamageEvent event){
+        if(event.getSource().getDirectEntity() instanceof AbstractArrow Arrow){
+            if(Arrow.getOwner() instanceof  Player player){
+                if(!player.level().isClientSide){
+                    final double[] mul = {1};
+
+                    player.getCapability(PlayerFoodArrowDamageBonusProvider.PLAYER_FOOD_ARROW_DAMAGE_BONUS_CAPABILITY).ifPresent(bonus->{
+
+                        mul[0] += bonus.getActual();
                     });
+                    MobEffect effect = TaotiesMunchies.DMG_UP.get();
+                    if (player.hasEffect(effect)) {
+                        mul[0] = mul[0] + (0.3*(player.getEffect(effect).getAmplifier()+1));
+                    }
+
+                    event.setAmount((float) (event.getAmount()*mul[0]));
+                    TaotiesMunchies.LOGGER.info(String.valueOf(event.getAmount()));
+
+
                 }
             }
 
-
-
-
-
-
-
-
-
-
-
+        }
     }
 }
